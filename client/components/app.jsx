7e1041072c578172d8c7de6e506635c4ce69a352
Cherie <cherietsukayama@gmail.com>
2020-04-18 03:53:38 +0000

    added header

---
+++ client/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container mt-4 mb-4'>
+        <header>
+          <h1>Student Grade Table</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
