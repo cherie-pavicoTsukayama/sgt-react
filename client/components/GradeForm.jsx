a1e76e6765aa685e5f2d224d032d7534e52e84d9
Cherie <cherietsukayama@gmail.com>
2020-04-18 23:50:52 +0000

    added handleChange method to the GradeForm component for each input field.

--- client/components/GradeForm.jsx
+++ client/components/GradeForm.jsx
@@ -8,22 +8,39 @@ export default class GradeForm extends React.Component {
       course: '',
       grade: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleChangeCourse = this.handleChangeName.bind(this);
+    // this.handleChangeName = this.handleChangeName.bind(this);
   }
+
+  handleChange(event) {
+    if (event.target.name === 'name') {
+      this.setState({ name: event.target.value });
+    }
+    if (event.target.name === 'course') {
+      this.setState({ course: event.target.value });
+    }
+    if (event.target.name === 'grade') {
+      this.setState({ grade: event.target.value });
+    }
+  }
+
+  handle
 
   render() {
     return (
-      <form className="ml-2">
+      <form className="ml-2 form-group">
         <div className="d-flex flex-nowrap align-items-center mb-3">
           <i className='fas fa-user col'></i>
-          <input type="text" placeholder="Name" name="name" />
+          <input type="text" placeholder="Name" name="name" onChange={ this.handleChange } />
         </div>
         <div className="d-flex flex-nowrap align-items-center mb-3">
           <i className='fas fa-book col'></i>
-          <input type="text" placeholder="Course" name="course" />
+          <input type="text" placeholder="Course" name="course" onChange={this.handleChange} />
         </div>
         <div className="d-flex flex-nowrap align-items-center mb-3">
           <i className='fas fa-graduation-cap col'></i>
-          <input type="text" placeholder="Grade" name="grade" />
+          <input type="text" placeholder="Grade" name="grade" onChange={this.handleChange} />
         </div>
         <div className="d-flex flex-nowrap justify-content-end">
           <button>Add</button>
