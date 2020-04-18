84a6545892a0c182a72982ec39f137acdca13062
Cherie <cherietsukayama@gmail.com>
2020-04-18 23:35:14 +0000

    revised the spacing for the icons and updated the icons to match the input.

--- client/components/GradeForm.jsx
+++ client/components/GradeForm.jsx
@@ -12,17 +12,17 @@ export default class GradeForm extends React.Component {
 
   render() {
     return (
-      <form className="ml-4">
+      <form className="ml-2">
         <div className="d-flex flex-nowrap align-items-center mb-3">
-          <i className='fas fa-user mr-3'></i>
+          <i className='fas fa-user col'></i>
           <input type="text" placeholder="Name" name="name" />
         </div>
         <div className="d-flex flex-nowrap align-items-center mb-3">
-          <i className='fas fa-user mr-3'></i>
+          <i className='fas fa-book col'></i>
           <input type="text" placeholder="Course" name="course" />
         </div>
         <div className="d-flex flex-nowrap align-items-center mb-3">
-          <i className='fas fa-user mr-3'></i>
+          <i className='fas fa-graduation-cap col'></i>
           <input type="text" placeholder="Grade" name="grade" />
         </div>
         <div className="d-flex flex-nowrap justify-content-end">
