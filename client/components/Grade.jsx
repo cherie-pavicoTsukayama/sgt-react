2e74aa41128ac6c5651c903d679e1ee3c9f6e11f
Cherie <cherietsukayama@gmail.com>
2020-04-19 16:10:41 +0000

    added align-middle to the table data

--- client/components/Grade.jsx
+++ client/components/Grade.jsx
@@ -7,10 +7,10 @@ export default function Grade(props) {
     props.deleteGrade(gradeId);
   }
   return (
-    <tr>
-      <td>{ grade.name }</td>
-      <td>{ grade.course}</td>
-      <td>{ grade.grade}</td>
+    <tr >
+      <td className="align-middle">{ grade.name }</td>
+      <td className="align-middle">{ grade.course}</td>
+      <td className="align-middle">{ grade.grade}</td>
       <td><button type="button" className="btn btn-danger" onClick={ handleClickDeleteGrade }>Delete</button></td>
     </tr>
   );
